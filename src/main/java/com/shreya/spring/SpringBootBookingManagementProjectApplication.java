2c4d245d2cf4b09e16a49d380c9a27c24af27f93
shreya mahalle <shreyamahalle2000@gmail.com>
2025-04-22 15:04:14 +0530

    spring boot project

---
+++ src/main/java/com/shreya/spring/SpringBootBookingManagementProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shreya.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookingManagementProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBookingManagementProjectApplication.class, args);
+	}
+
+}
