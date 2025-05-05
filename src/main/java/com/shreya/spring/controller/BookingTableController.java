050f7f92c962b35dfb4ca5642e8b7378364e078c
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-05 17:37:09 +0530

    updated

--- src/main/java/com/shreya/spring/controller/BookingTableController.java
+++ src/main/java/com/shreya/spring/controller/BookingTableController.java
@@ -20,30 +20,30 @@ public class BookingTableController {
     @Autowired
     private BookingTableService bookingTableService;
 
-    @PostMapping("/tableBooking")
+    @PostMapping("/table")
     public boolean addBooking(@RequestBody BookingTable bookingTable) throws SQLException {
         log.info("Received request to add booking: {}", bookingTable);
         return bookingTableService.addBooking(bookingTable);
     }
 
-    @GetMapping("/tableBookings")
+    @GetMapping("/table")
     public List<BookingTable> getAllBookings() {
         log.info("Received request to fetch all bookings");
         return bookingTableService.getAllBookings();
     }
 
-    @GetMapping("/tableBooking/{id}")
+    @GetMapping("/table/{id}")
     public BookingTable getBookingById(@PathVariable("id") Long id) {
         log.info("Received request to fetch booking by ID: {}", id);
         return bookingTableService.getBookingById(id);
     }
 
-    @PutMapping("/tableBooking/{id}")
+    @PutMapping("/table/{id}")
     public boolean updateBooking(@PathVariable Long id, @RequestBody BookingTable bookingTable) {
         log.info("Received request to update booking with ID: {}. New data: {}", id, bookingTable);        return bookingTableService.updateBooking(id, bookingTable);
     }
 
-    @DeleteMapping("/tableBooking/{id}")
+    @DeleteMapping("/table/{id}")
     public boolean deleteBooking(@PathVariable Long id) {
         log.info("Received request to delete booking with ID: {}", id);
         return bookingTableService.deleteBooking(id);
