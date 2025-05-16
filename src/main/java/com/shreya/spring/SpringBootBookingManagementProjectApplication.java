b6dd45f1fe9d23569d9db950fbf24e4ac7452011
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-16 15:34:44 +0530

    updated

--- src/main/java/com/shreya/spring/SpringBootBookingManagementProjectApplication.java
+++ src/main/java/com/shreya/spring/SpringBootBookingManagementProjectApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.sql.SQLException;
 
 @SpringBootApplication
-
 public class SpringBootBookingManagementProjectApplication {
 
     public static void main(String[] args) throws InvalideCustomerIDException, SQLException {
